50671bd5acc2cb970a9d1402e2ce65658847de8a
clementG <clement.goachet@gmail.com>
2016-02-24 14:49:03 +0100

    factory

--- js/pokedex.js
+++ js/pokedex.js
@@ -4,34 +4,54 @@ pokeApp.config(['$resourceProvider', function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
 }]);
 
-var pokeApiUrl = "http://pokeapi.co/"
+var pokeApiUrl = "http://pokeapi.co/";
 
-pokeApp.controller('PokemonList', function($scope, $log, $http, $resource) {
+pokeApp.factory('DetailFact', function($resource) {
+	var factory = {};
+
+	factory.get = function(id){
+		var get_pokemon = $resource(pokeApiUrl + "api/v2/pokemon/:pokemonId", {pokemonId: "@pokemonId"});
+		get_pokemon.get({pokemonId: id}, function(response) {
+		console.log(response);
+			return response;
+		});
+	};
+	
+	return factory;
+});
+
+pokeApp.service('DetailServ', function(DetailFact){
+	this.detail = function(id) {
+		return DetailFact.get(id);
+	}
+});
+
+
+pokeApp.controller('PokemonList', function($scope, $log, $http, $resource, DetailServ) {
 
     $scope.names = [];
 
-    var get_pokemons = $resource(pokeApiUrl + "api/v2/pokedex/1")
+    var get_pokemons = $resource(pokeApiUrl + "api/v1/pokedex/1")
     get_pokemons.get(function(response) {
         for(var i=0; i < response.pokemon.length; i++) {
             $scope.names.push(response.pokemon[i].name)
         }
     });
+	
     
     $scope.go = function (id) {
         var abilities = {};
 
-        var get_pokemon = $resource(pokeApiUrl + "api/v2/pokemon/:pokemonId", {pokemonId: "@pokemonId"});
-        get_pokemon.get({pokemonId: id}, function(response) {
-            $scope.info = {
-                "id": response.id,
-                "nom": response.name,
-                "expérience": response.base_experience,
-                "hauteur": response.height,
-                "poids": response.weight
-            };
+		var pokemon = DetailServ.detail(id);
+		/*$scope.info = {
+			"id": pokemon.id,
+			"nom": pokemon.name,
+			"expérience": pokemon.base_experience,
+			"hauteur": pokemon.height,
+			"poids": pokemon.weight
+		};*/
 
-        });
-
-        var get_abilities = $resource(pokeApiUrl + "api/v2/ability/:abilityId", {abilityId: "@abilityId"});
+        //var get_abilities = $resource(pokeApiUrl + "api/v2/ability/:abilityId", {abilityId: "@abilityId"});
     }
 });
+

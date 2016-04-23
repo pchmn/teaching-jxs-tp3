42d84d2105ec905a5f43a376c6b52347f4e89761
ClementG <Clement.goachet@gmail.com>
2016-04-23 12:23:50 +0200

    Can access to the data stored by the service

--- js/pokedex.js
+++ js/pokedex.js
@@ -8,12 +8,15 @@ pokeApp.config(['$resourceProvider', function($resourceProvider) {
 var pokeApiUrl = "http://pokeapi.co/";
 
 // factory pour récupérer les infos d'un pokemon
-pokeApp.factory('DetailFact', function($resource) {
+pokeApp.factory('DetailFact', function($resource, myService) {
 	var factory = {};
 
 	factory.getBase = function(id, callback){
+
 		var get_pokemon = $resource(pokeApiUrl + "api/v2/pokemon/:pokemonId", {pokemonId: "@pokemonId"});
 		get_pokemon.get({pokemonId: id}, function(response) {
+			console.log(response);
+			myService.setData(response);
 			callback(response);
 		});
 	};
@@ -27,6 +30,31 @@ pokeApp.factory('DetailFact', function($resource) {
     }
 
 	return factory;
+});
+
+// factory pour récupérer les infos d'un pokemon
+pokeApp.factory('myService', function() {
+    var infos = {};
+
+    return {
+        getData: function () {
+            //You could also return specific attribute of the form data instead
+            //of the entire data
+            return infos;
+        },
+        setData: function (newInfos) {
+            //You could also set specific attribute of the form data instead
+            infos.id = newInfos.id;
+            infos.name = newInfos.name;
+            infos.order = newInfos.order;
+            infos.weight = newInfos.weight;
+            infos.height = newInfos.height;
+        },
+        resetData: function () {
+            //To be called when the data stored needs to be discarded
+            infos = {};
+        }
+    };
 });
 
 pokeApp.factory('Pokedex', function($resource){
@@ -47,26 +75,35 @@ pokeApp.factory('Pokedex', function($resource){
 });
 
 // controller pour la liste des pokemons
-pokeApp.controller('PokemonList', function($scope, Pokedex) {
+pokeApp.controller('PokemonList', function($scope, Pokedex, DetailFact) {
 
     $scope.names = [];
 
 	Pokedex.getAll(function(element){
 		$scope.names = element;
 	});
-});
-
-pokeApp.controller('PokemonInfos', function($scope, DetailFact) {
 
 	$scope.go = function (id) {
+		console.log('Go !');
 		DetailFact.getBase(id, function(element){
-			$scope.info = {
-				"id": element.id,
-				"nom": element.name,
-				"expérience": element.base_experience,
-				"hauteur": element.height,
-				"poids": element.weight
-			};
+			$scope.finded = "ok";
 		});
 	}
+});
+
+pokeApp.controller('PokemonInfos', function($scope, myService) {
+
+	$scope.service = myService;
+
+	$scope.display = function(results) {
+		$scope.info = results;
+	}
+
+	$scope.$watch(
+		function(){
+			return myService.getData();
+		}, function(newVal, oldVal){
+			$scope.info = myService.getData();
+		}, true
+	);
 });

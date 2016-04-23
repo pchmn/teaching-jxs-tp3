40e954342bbd53c1c86a1193b15a663b01f539bd
ClementG <Clement.goachet@gmail.com>
2016-04-23 17:44:29 +0200

    Chargement des sprites ok

--- js/pokedex.js
+++ js/pokedex.js
@@ -8,7 +8,7 @@ pokeApp.config(['$resourceProvider', function($resourceProvider) {
 var pokeApiUrl = "http://pokeapi.co/";
 
 // factory pour récupérer les infos d'un pokemon
-pokeApp.factory('DetailFact', function($resource, myService) {
+pokeApp.factory('DetailFact', function($resource) {
 	var factory = {};
 
 	factory.getBase = function(id, callback){
@@ -16,7 +16,6 @@ pokeApp.factory('DetailFact', function($resource, myService) {
 		var get_pokemon = $resource(pokeApiUrl + "api/v2/pokemon/:pokemonId", {pokemonId: "@pokemonId"});
 		get_pokemon.get({pokemonId: id}, function(response) {
 			console.log(response);
-			myService.setData(response);
 			callback(response);
 		});
 	};
@@ -35,6 +34,7 @@ pokeApp.factory('DetailFact', function($resource, myService) {
 // factory pour récupérer les infos d'un pokemon
 pokeApp.factory('myService', function() {
     var infos = {};
+    var image = {};
 
     return {
         getData: function () {
@@ -42,13 +42,16 @@ pokeApp.factory('myService', function() {
             //of the entire data
             return infos;
         },
+        getImage: function() {
+        	return image;
+        },
         setData: function (newInfos) {
+        	console.log(newInfos);
             //You could also set specific attribute of the form data instead
             infos.id = newInfos.id;
             infos.name = newInfos.name;
-            infos.order = newInfos.order;
-            infos.weight = newInfos.weight;
-            infos.height = newInfos.height;
+            infos.abilities = newInfos.abilities;
+            image.url = newInfos.sprites.back_default;
         },
         resetData: function () {
             //To be called when the data stored needs to be discarded
@@ -63,9 +66,10 @@ pokeApp.factory('Pokedex', function($resource){
 	factory.getAll = function(callback){
 		var get_all_pokemon = $resource(pokeApiUrl + "api/v2/pokedex/1");
 		get_all_pokemon.get(function(response) {
-			var pokedex = {};
+			var pokedex = [];
 			for(var i=0; i < response.pokemon_entries.length; i++) {
-				pokedex[response.pokemon_entries[i].entry_number] = response.pokemon_entries[i].pokemon_species.name;
+				var value = {entry_number:response.pokemon_entries[i].entry_number, name:response.pokemon_entries[i].pokemon_species.name}
+				pokedex.push(value);
 			};
 			callback(pokedex);
 		});
@@ -74,19 +78,20 @@ pokeApp.factory('Pokedex', function($resource){
 	return factory;
 });
 
+
 // controller pour la liste des pokemons
-pokeApp.controller('PokemonList', function($scope, Pokedex, DetailFact) {
+pokeApp.controller('PokemonList', function($scope, Pokedex, DetailFact, myService) {
 
     $scope.names = [];
 
 	Pokedex.getAll(function(element){
-		$scope.names = element;
+		$scope.pokemons = element;
 	});
 
 	$scope.go = function (id) {
 		console.log('Go !');
 		DetailFact.getBase(id, function(element){
-			$scope.finded = "ok";
+			myService.setData(element);
 		});
 	}
 });
@@ -99,23 +104,35 @@ pokeApp.controller('PokemonInfos', function($scope, myService) {
 		$scope.info = results;
 	}
 
-	$scope.$watch(
-		function(){
-			return myService.getData();
-		}, function(newVal, oldVal){
-			$scope.info = myService.getData();
-		}, true
-	);
+
 });
 
-pokeApp.directive('pokedex', function() {
+pokeApp.directive('pokedex', function(myService) {
 	var directive = {};
 	directive.restrict = 'E';
-	directive.templateUrl = "pokedex.html";
+	directive.templateUrl = "directive/pokedex.html";
 
-	directive.compile = function(element, attributes) {
-	   element.css("border", "1px solid #cccccc");
+
+	directive.link = function($scope, $elem, attrs){
+		$scope.$watch(
+			function(){
+				return myService.getData();
+			}, function(newVal, oldVal){
+				$scope.info = myService.getData();
+				$scope.image = myService.getImage();
+			}, true
+		);
 	}
 
+	return directive;
+});
+
+pokeApp.directive('pokedexSearch', function() {
+	var directive = {};
+	directive.restrict = 'E';
+	directive.templateUrl = "directive/pokedexSearch.html";
+	directive.link = function($scope, $elem, attrs){
+
+	}
 	return directive;
 });
